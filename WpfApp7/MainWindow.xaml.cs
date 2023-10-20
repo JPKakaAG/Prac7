38890f36aa6d1c1c0dbc1b295df7729676256512
JPKakaAG <Gjopka@yandex.ru>
2023-10-20 15:54:11 +0300

    update

--- WpfApp7/MainWindow.xaml.cs
+++ WpfApp7/MainWindow.xaml.cs
@@ -24,7 +24,93 @@ namespace WpfApp7
         {
             InitializeComponent();
         }
+        private void btnExit_click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
 
+        private void btnInfo_click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Выполнил лучший разработчик: Девяткин Вадим Евгеньевич\r\nПрактическая №6\r\nИспользовать класс Pair (пара четных чисел). Разработать операцию перемножения \r\nпар (а, b) * (с, d) = (а * c, b * d). Разработать операцию инкремента для удвоения \r\nпары чисел.");
+        }
 
+        private void btnDa_click(object sender, RoutedEventArgs e)
+        {
+            string result1 = tbA.Text.Replace(" ", "");
+            string result2 = tbB.Text.Replace(" ", "");
+            if (int.TryParse(result1, out int A) && int.TryParse(result2, out int B))
+            {
+                string userInput1 = tbA.Text;
+                string userInput2 = tbB.Text;
+                string[] numbersArray1 = userInput1.Split(' ');
+                string[] numbersArray2 = userInput2.Split(' ');
+
+                if (numbersArray1.Length == 2 && numbersArray2.Length == 2)
+                {
+                    int.TryParse(numbersArray1[0], out int a);
+                    int.TryParse(numbersArray1[1], out int b);
+                    int.TryParse(numbersArray2[0], out int c);
+                    int.TryParse(numbersArray2[1], out int d);
+
+                    Pair pair1 = new Pair(a, b);
+                    Pair pair2 = new Pair(c, d);
+                    Pair multiplicationResult = pair1 * pair2;
+                    tbRes1.Text = ($"Перемножениe пар {a}*{c}, {b}*{d} = {multiplicationResult.First}, {multiplicationResult.Second}");
+
+                    Pair incrementResult = ++pair1;
+                    tbRes2.Text = ($"Результат инкремента ({incrementResult.First}, {incrementResult.Second})");
+                }
+                else
+                {
+                    MessageBox.Show("Введите по 2 числа через пробел например: 1 2");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите числа");
+            }
+        }
+
+        private void btnMediana_clickB(object sender, RoutedEventArgs e)
+        {
+            gr2.Visibility = Visibility.Hidden;
+            gr1.Visibility = Visibility.Visible;
+        }
+        private void btnMediana_click(object sender, RoutedEventArgs e)
+        {
+            gr1.Visibility = Visibility.Hidden;
+            gr2.Visibility = Visibility.Visible;
+        }
+
+        private void btnDaB_click(object sender, RoutedEventArgs e)
+        {
+            string result1 = tbAB.Text.Replace(" ", "");
+            if (int.TryParse(result1, out int A))
+            {
+                string userInput1 = tbAB.Text;
+                string[] numbersArray1 = userInput1.Split(' ');
+
+                if (numbersArray1.Length == 2)
+                {
+                    int.TryParse(numbersArray1[0], out int a);
+                    int.TryParse(numbersArray1[1], out int b);
+
+                    RightAngledTriangle triangle = new RightAngledTriangle(a, b);
+                    double hypotenuse = triangle.CalculateHypotenuse();
+                    double area = triangle.CalculateArea();
+                    tbRes1B.Text = ($"Гипотенуза треугольника: {hypotenuse}");
+
+                    tbRes2B.Text = ($"Площадь треугольника: {area}");
+                }
+                else
+                {
+                    MessageBox.Show("Введите по 2 числа через пробел например: 1 2");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите числа");
+            }
+        }
     }
 }
---
+++ WpfApp7/Pair.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp7
+{
+    public class Pair
+    {
+        public int First { get; set; }
+        public int Second { get; set; }
+
+        /// <summary>
+        /// Конструктор, принимающий значения для обоих чисел в паре
+        /// </summary>
+        /// <param name="first"> первое число из пары</param>
+        /// <param name="second"> второе число из пары</param>
+        public Pair(int first, int second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        /// <summary>
+        /// Перегрузка оператора умножения для пар
+        /// </summary>
+        /// <param name="a">первое число из второй пары</param>
+        /// <param name="b">второе число из второй пары</param>
+        /// <returns>возвращает результат перемножения</returns>
+        public static Pair operator *(Pair a, Pair b)
+        {
+            int resultFirst = a.First * b.First;
+            int resultSecond = a.Second * b.Second;
+            return new Pair(resultFirst, resultSecond);
+        }
+
+        /// <summary>
+        /// Перегрузка оператора инкремента для удвоения пары чисел
+        /// </summary>
+        /// <param name="p">объект</param>
+        /// <returns>возвращает результат удвоения пары чисел</returns>
+        public static Pair operator ++(Pair p)
+        {
+            int doubledFirst = p.First * 2;
+            int doubledSecond = p.Second * 2;
+            return new Pair(doubledFirst, doubledSecond);
+        }
+    }
+    public class RightAngledTriangle : Pair
+    {
+        public RightAngledTriangle(int first, int second) : base(first, second)
+        {
+        }
+
+        /// <summary>
+        /// Метод для вычисления гипотенузы треугольника
+        /// </summary>
+        /// <returns>гипотенуза</returns>
+        public double CalculateHypotenuse()
+        {
+            double hypotenuse = Math.Sqrt(First * First + Second * Second);
+            return hypotenuse;
+        }
+
+        /// <summary>
+        /// Метод для вычисления площади треугольника
+        /// </summary>
+        /// <returns>площадь</returns>
+        public double CalculateArea()
+        {
+            double area = 0.5 * First * Second;
+            return area;
+        }
+    }
+}
